654b4aeb3d181563853a051d1e7ac603e92a2ecf
Benjamin Zethmayr <bzethmayr@users.noreply.github.com>
2018-10-08 13:58:26 -0500

    Update package-info.java
    Takes up a change only present in README.md.
    TODO: Generate README.md from this file

--- src/main/java/net/zethmayr/benjamin/spring/common/package-info.java
+++ src/main/java/net/zethmayr/benjamin/spring/common/package-info.java
@@ -2,6 +2,7 @@
  * Things helpful for simple Spring Boot JDBC web applications,
  * including: <ul>
  *     <li>a trivial ORM implementation,</li>
+ *     <li>declarative creation of CSV reports,</li>
  *     <li>a hierarchical command parser,</li>
  *     <li>and some other stuff</li>
  * </ul>
